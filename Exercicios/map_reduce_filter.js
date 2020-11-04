dc6dfe0d4ed2dfe8620d7df72edece1ee3667491
Lucas Cherobim <lucascherobim@hotmail.com>
2020-11-04 09:05:26 -0300

    update files

--- Exercicios/map_reduce_filter.js
+++ Exercicios/map_reduce_filter.js
@@ -6,7 +6,9 @@ const carrinho = [
     { nome: "Tesoura", qtde: 1, preco: 19.2 },
     ];
 
+    //Araay.prototype -> O construtor de protótipo permite adicionar novas propriedades e métodos ao objeto Array ().
 
+    //filter -> Cria um novo array com todos os elementos do array para qual a função de filtragem fornecida retorne true.
     Array.prototype.meuFilter = function(fn) {
         const filtered = []
         for(let el of this ){
@@ -16,8 +18,37 @@ const carrinho = [
         }
         return filtered
     }
-    
-    const qtdMaiorQueZero = item => item.qtde > 0 ;
-    const meuFilter = carrinho.meuFilter(qtdMaiorQueZero);
 
-    console.log(meuFilter);
+    //map -> Cria um novo array com os resultados da função fornecida chamada em cada elemento na array.
+    Array.prototype.meuMap = function(fn){
+        const mapped = []
+        for (let el of this){
+            mapped.push(fn(el))
+        }
+        return mapped
+    }
+
+    //reduce -> Aplica uma função contra um acumulador e cada valor do array (da esquerda para direita) para reduzi-los a um único valor.
+    Array.prototype.meuReduce = function(fn, inicial){
+        let ultimo = inicial
+        for(let el of this){
+            if(!ultimo){
+                ultimo = el
+                continue
+            }
+            ultimo = fn(ultimo, el)
+        }
+        return ultimo
+    }
+    
+    const qtdMaiorQueZero = item => item.qtde > 0 
+    const totalItem = item => item.qtde * item.preco
+    const somar  = (a,b) => a + b
+
+    const resultado = carrinho
+        .meuFilter(qtdMaiorQueZero)
+        .meuMap(totalItem)
+        .meuReduce(somar)
+
+
+    console.log(resultado);
