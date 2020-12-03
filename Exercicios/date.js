a947c714a742706c74d143f02f267b6889d3448f
Lucas Cherobim <lucascherobim@hotmail.com>
2020-12-02 23:12:18 -0300

    update files

---
+++ Exercicios/date.js
@@ -0,0 +1,4 @@
+var d = new Date();
+console.log(d.getDate());//dia do mes
+console.log(d.getDay());//dia da semana
+console.log(d.getTime());//retorna o valor numerico da contagem de dias desde 1970
