b8677653d0a35d7b86d174649d8921fcf752ee55
Lucas Cherobim <lucascherobim@hotmail.com>
2020-12-09 21:54:38 -0300

    update files

---
+++ Exercicios/modules/index.js
@@ -0,0 +1,10 @@
+// import {areaQuadrado, perimetroQuadrado} from "./quadrado.js"
+import aleatorio from "./aleatorio.js"
+
+import Quadrado from "./quadrado.js"
+
+console.log(Quadrado.area(3));
+console.log(Quadrado.perimetro(4));
+console.log(Quadrado.aleatorio());
+// console.log(perimetroQuadrado(6));
+// console.log(aleatorio());
